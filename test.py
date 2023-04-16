75e718d2f39de747d077fff57169e3012e59faed
Gideon <gideonogilvie@gmail.com>
2023-04-16 23:06:28 +0200

    report

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import math
 import matplotlib.pyplot as plt
 
-def cyclical_learning_rate(input_epoch, lr):
+def cyclical_learning_rate(epoch, lr):
     base_lr = 0.001  # The minimum learning rate
     max_lr = 0.01    # The maximum learning rate
     step_size = 3   # The number of epochs between each cycle
-
-    epoch = input_epoch + step_size - 1
 
     cycle = math.floor(1 + epoch/(2*step_size))
     x = abs(epoch/step_size - 2*cycle + 1)
@@ -24,6 +22,10 @@ y[0] = cyclical_learning_rate(1, 0.001)
 for nr in x[1:]:
     y[nr] = cyclical_learning_rate(nr, y[nr-1])
 
+plt.plot(y)
+plt.xlabel("epochs")
+plt.ylabel("learning rate")
+plt.show()
 
 plt.savefig("Stanford", format='png')
 
